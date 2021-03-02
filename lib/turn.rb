84c8f30550f6e7a00f1c7bef52b249365b058f61
Austin Johnson <technicallyaustin@gmail.com>
2021-03-02 17:26:59 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,7 +13,7 @@ def turn(board)
   index = input_to_index(user_input)
   if valid_move?(board, index)
     #move(board,index, current_player(board))
-    turn(board)
+    #turn(board)
   end
   display_board(board)
 end
