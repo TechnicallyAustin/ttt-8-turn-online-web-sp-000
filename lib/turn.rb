2370011adf7ee1d5b34523aaafccabee313e6603
Austin Johnson <technicallyaustin@gmail.com>
2021-03-02 17:32:22 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -20,6 +20,16 @@ def turn(board)
   end
   
 end
+
+def turn_count(board)
+   counter = 0
+   board.each do |spaces|
+      if spaces == "X" || spaces == "O"
+         counter += 1
+      end
+   end
+   counter
+end
     
 def player(board)
    turn_count(board) % 2 == 0 ? "X" : "O"
